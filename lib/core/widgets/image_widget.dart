30e51043779521bbfd975dcf94f18f316c8906c4
João Sereia <joao.lo.sereia@gmail.com>
2024-10-01 14:10:05 -0300

    feat(widgets): adicionar widget de imagem via url ou asset

---
+++ lib/core/widgets/image_widget.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:the_menu/core/mixins/theme_mixin.dart';
+
+class ImageWidget extends StatelessWidget with ThemeMixin {
+  const ImageWidget({
+    required this.uri,
+    this.fit = BoxFit.cover,
+    this.width,
+    this.height,
+    super.key,
+  });
+
+  final String uri;
+  final BoxFit fit;
+  final double? width;
+  final double? height;
+
+  @override
+  Widget build(BuildContext context) {
+    final metrics = getMetrics(context);
+
+    ImageProvider provider = NetworkImage(uri);
+    if (uri.contains('assets')) {
+      provider = AssetImage(uri);
+    }
+
+    return ClipRRect(
+      borderRadius: BorderRadius.all(metrics.radius / 2),
+      child: Image(
+        width: width,
+        height: height,
+        image: provider,
+        fit: fit,
+      ),
+    );
+  }
+}
